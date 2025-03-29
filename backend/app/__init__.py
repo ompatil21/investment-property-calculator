d91e3ae490d75490b3a23feed46d2fc30f5f756a
ompatil21 <officialompatil@gmail.com>
2025-03-29 19:20:13 +1100

    perperty submit module initial form logic

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,13 @@
-from flask import app
-from app.routes import properties
+from flask import Flask
+from flask_cors import CORS
 
-app.register_blueprint(properties.bp)
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    from app.routes import properties
+
+    app.register_blueprint(properties.bp)
+
+    return app
